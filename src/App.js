1dfe83720b44770b92dcbb017d2bc8a3ca7be7da
Leticia <lettycuevas91@gmail.com>
2023-01-29 20:54:32 -0300

    update app.js
    solucion app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+   constructor(props){
+    super(props);
+    this.state = {
+      users: [],
+      name: '',
+      lname: ''
+    }
+  }
+
+  addUser(event){
+    event.preventDefault()
+    this.setState({
+      users: this.state.users.concat({name: this.state.name, lname: this.state.lname}),
+      name: '',
+      lname: ''
+     });
+  }
+
+  updateUsername(event){
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  updateUserlname(event){
+    this.setState({
+      lname: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addUser.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name"value={this.state.name} onChange={this.updateUsername.bind(this)} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lname} onChange={this.updateUserlname.bind(this)} />
               </div>
 
               <div className="action">
@@ -30,7 +60,14 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              <tr>
+              <td>
+                {this.state.users.map((user, index) => <li key={user.id}>{user.name}</li>)} 
+              </td>
+              <td>
+                {this.state.users.map((user, index) => <li key={user.id}>{user.lname}</li>)} 
+              </td>
+              </tr>
               </tbody>
             </table>
           </div>
